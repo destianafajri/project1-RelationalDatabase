11205ac11efce5b3b171cd3501f7be4d9313cef5
DestianaFR <109920172+destianafajri@users.noreply.github.com>
2023-05-07 23:29:43 +0700

    Update Syntax_DDL.sql

--- Syntax_DDL.sql
+++ Syntax_DDL.sql
@@ -24,9 +24,12 @@ CREATE TABLE pengguna
 	id_kota integer not null,
 	first_name varchar(50) not null,
 	last_name varchar(50) not null,
-	phone_number int not null,
+	phone_number numeric not null,
 	user_type varchar(10) not null,
-	alamat text not null
+	alamat text not null,
+	CONSTRAINT fk_id_kota
+		FOREIGN KEY(id_kota)
+		REFERENCES kota(id_kota)
 );
 
 -- mengubah tipe data pada kolom phone_number di tabel pengguna --
@@ -74,7 +77,10 @@ CREATE TABLE buyer
 	id_produk integer not null,
 	date_bid date not null,
 	bid_price numeric not null,
-	bid_status varchar(10) not null
+	bid_status varchar(10) not null,
+	CONSTRAINT fk_id_user
+		FOREIGN KEY(id_user)
+		REFERENCES pengguna(id_user)
 );
 
 -- membuat tabel katalog --
@@ -84,7 +90,13 @@ CREATE TABLE katalog
 	id_user integer not null,
 	id_produk integer not null,
 	deskripsi_produk text,
-	status_produk varchar(10) not null
+	status_produk varchar(10) not null,
+	CONSTRAINT fk_id_user
+		FOREIGN KEY(id_user)
+		REFERENCES pengguna(id_user),
+	CONSTRAINT fk_id_produk
+		FOREIGN KEY(id_produk)
+		REFERENCES produk(id_produk)
 );
 
 -- menambah kolom baru pada tabel katalog --
@@ -93,9 +105,13 @@ ADD COLUMN tgl_posting date
 
 -- update data pada kolom tgl_posting di tabel katalog --
 UPDATE katalog SET tgl_posting = '2022-05-14' WHERE id_katalog = 1
+
 UPDATE katalog SET tgl_posting = '2023-01-12' WHERE id_katalog = 2
+
 UPDATE katalog SET tgl_posting = '2021-12-30' WHERE id_katalog = 3
+
 UPDATE katalog SET tgl_posting = '2019-03-04' WHERE id_katalog = 4
+
 UPDATE katalog SET tgl_posting = '2022-09-10' WHERE id_katalog = 5
 
 		
